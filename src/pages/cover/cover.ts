5b45f07c49eb48718208b2c4ef127d480498b212
AlexRingrose <43159722+AlexRingrose@users.noreply.github.com>
2018-10-05 19:33:55 -0700

    Attempting to add forms

--- src/pages/cover/cover.ts
+++ src/pages/cover/cover.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import {NavController, NavParams } from 'ionic-angular';
-
 /**
  * Generated class for the CoverPage page.
  *
@@ -13,8 +12,8 @@ import {NavController, NavParams } from 'ionic-angular';
   templateUrl: 'cover.html',
 })
 export class CoverPage {
-
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
   }
 
   ionViewDidLoad() {
