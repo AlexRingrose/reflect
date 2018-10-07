f0ebdfc8d9e5d994692d22290836bd088e6c1335
AlexRingrose <43159722+AlexRingrose@users.noreply.github.com>
2018-10-06 19:45:11 -0700

    Created basic forms with start at validation

--- src/pages/cover/cover.ts
+++ src/pages/cover/cover.ts
@@ -1,23 +1,42 @@
 import { Component } from '@angular/core';
 import {NavController, NavParams } from 'ionic-angular';
-/**
- * Generated class for the CoverPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
+import { HomePage } from '../../pages/home/home';
 
 @Component({
   selector: 'page-cover',
   templateUrl: 'cover.html',
 })
 export class CoverPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  validations_form: FormGroup;
+  constructor(public navCtrl: NavController,public formBuilder: FormBuilder) {}
 
+    ionViewWillLoad(){
+      this.validations_form = this.formBuilder.group({
+        name: new FormControl('', Validators.required),
+        id: new FormControl('', Validators.required)
+      })
+    }
+
+    validation_messages = {
+      'name' : [
+        { type: 'required', message: 'Name is required'}
+      ],
+      'id' : [
+        { type: 'required', message: 'Name is required'}
+      ]
+    };
+
+
+    ionViewDidLoad(){
+      console.log('ionViewDidLoad CoverPage')
+    }
+
+  onSubmit(values){
+    console.log(values)
+    //this.navCtrl.push(HomePage);
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CoverPage');
-  }
 
 }
