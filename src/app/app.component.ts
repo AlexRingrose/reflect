6ddbcc2d9a57f5e49d05cd28938fd4d0dc101137
AlexRingrose <43159722+AlexRingrose@users.noreply.github.com>
2018-10-13 16:19:14 -0700

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 import { CoverPage } from '../pages/cover/cover';
 import { PreviewPage} from '../pages/preview/preview';
 
